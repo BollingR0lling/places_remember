c1390ce393997f48a37136990ad45671d6830a47
Вован Кожемяченко <korovka11sving@gmail.com>
2020-11-29 20:51:58 +0500

    feat: Added memory model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Memory(models.Model):
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    location = models.CharField(max_length=255, null=False)
+    title = models.CharField(max_length=50, unique=True, null=False, verbose_name='Воспоминание')
+    description = models.TextField(max_length=1024, null=False, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.user.first_name, self.user.last_name}, воспоминание: {self.title}'
