5a0024feeb333afba9ddf12b4317556fd43b000b
Вован Кожемяченко <korovka11sving@gmail.com>
2020-12-01 19:35:55 +0500

    feat: Added map with memory-place marks on home page, validation location

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,8 +1,41 @@
-from django.forms import ModelForm
+from django.forms import (
+    ModelForm,
+    CharField,
+    Textarea,
+    TextInput,
+    ValidationError
+)
+from geopy import Nominatim
+
 from .models import Memory
 
 
 class MemoryModelForm(ModelForm):
+    location = CharField(
+        label='',
+        widget=TextInput(attrs={"placeholder": "Memory place"}))
+
+    title = CharField(
+        label='',
+        widget=TextInput(attrs={"placeholder": "Your memory"})
+    )
+    description = CharField(
+        label='',
+        widget=Textarea(attrs={
+            "placeholder": "Describe your memory"
+        })
+    )
+
     class Meta:
         model = Memory
         fields = ('location', 'title', 'description')
+
+    def clean_location(self, *args, **kwargs):
+        location = self.cleaned_data.get('location')
+        geolocator = Nominatim(user_agent='Mozilla/5.0')
+        try:
+            place = geolocator.geocode(location)
+            p_lat, p_lon = place.latitude, place.longitude
+            return location
+        except AttributeError:
+            raise ValidationError("This place is not exists")
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from geopy.geocoders import Nominatim
 from .forms import MemoryModelForm
 from .models import Memory
 import folium
@@ -11,8 +12,21 @@ def login(request):
 
 @login_required
 def home(request):
+    m = folium.Map(width=1250, height=750, zoom_start=13)
+    memories = Memory.objects.filter(user=request.user)
+    if memories:
+        for memory in memories:
+            geolocator = Nominatim(user_agent='Mozilla/5.0')
+            location = geolocator.geocode(memory.location)
+            folium.Marker(
+                [location.latitude, location.longitude],
+                popup=f'{memory.title}',
+                icon=folium.Icon(color='red', icon='info-sign')
+            ).add_to(m)
+    html_map = m._repr_html_()
     context = {
-        'memories': Memory.objects.filter(user=request.user),
+        'memories': memories,
+        'map': html_map,
     }
     return render(request, 'home.html', context)
 
@@ -21,15 +35,16 @@ def home(request):
 def add_memory(request):
     form = MemoryModelForm(request.POST or None)
     m = folium.Map(width=1250, height=750)._repr_html_()
-
     if form.is_valid():
+        location = form.cleaned_data.get('location')
         memory = Memory.objects.create(
             user=request.user,
-            location=form.cleaned_data.get('location'),
+            location=location,
             title=form.cleaned_data.get('title'),
             description=form.cleaned_data.get('description')
         )
         memory.save()
+
         return home(request)
 
     context = {
