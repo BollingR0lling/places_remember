541f804d3c0fcf3fe3b10bb482dffc7ee55d9998
Вован Кожемяченко <korovka11sving@gmail.com>
2020-12-05 15:29:22 +0500

    Configured CI Server

--- places_remember/settings.py
+++ places_remember/settings.py
@@ -84,8 +84,6 @@ WSGI_APPLICATION = 'places_remember.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_db',
         'USER': 'django',
